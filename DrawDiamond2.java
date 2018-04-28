7adbea5539715f207ee8c94bad01ba48838382be
shirleyechi <38809346+shirleyechi@users.noreply.github.com>
2018-04-28 18:47:29 +0800

    DrawDiamond2.java

---
+++ DrawDiamond2.java
@@ -0,0 +1,18 @@
+public class print {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("    *    ");
+		System.out.println("   * *   ");
+		System.out.println("  * * *  ");
+		System.out.println(" * * * * ");
+		System.out.println("* * * * *");
+		System.out.println(" * * * * ");
+		System.out.println("  * * *  ");
+		System.out.println("   * *   ");
+		System.out.println("    *    ");
+		
+	}
+
+}
